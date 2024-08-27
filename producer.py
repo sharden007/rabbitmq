b4d0da65e049a441a1bb73b85dfee2211f112592
Shawn Harden <sharden007@gmail.com>
2024-08-27 15:43:01 -0400

    Added additional messages, threading

--- producer.py
+++ producer.py
@@ -1,4 +1,5 @@
 import pika
+import time
 
 # Establish a connection to RabbitMQ server
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672))
@@ -7,9 +8,12 @@ channel = connection.channel()
 # Declare a queue named 'hello'
 channel.queue_declare(queue='hello')
 
-# Send a message to the 'hello' queue
-channel.basic_publish(exchange='', routing_key='hello', body='Hello RabbitMQ!')
-print(" [x] Sent 'Hello RabbitMQ!'")
+# Send 20 messages to the 'hello' queue with the format 'Hello RabbitMQ#n'
+for i in range(20):
+    message = f'Hello RabbitMQ#{i+1}'
+    channel.basic_publish(exchange='', routing_key='hello', body=message)
+    print(f" [x] Sent '{message}'")
+    time.sleep(5)  # Wait for 5 seconds before sending the next message
 
 # Close the connection
 connection.close()
